966dc39511702a6d00a427efb4304c7e274932d6
Begimai A <begimai.akulova@gmail.com>
2020-07-29 22:53:46 +0300

    Inserted new files

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$('.questions__link1').click(function() {
+    var popup_id=$('#'+$(this).attr("rel"));
+    $(popup_id).show();
+    $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+    $('.overlay_popup, .answer').hide();
+})
+
+$('.questions__link2').click(function() {
+    var popup_id=$('#'+$(this).attr("rel"));
+    $(popup_id).show();
+    $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+    $('.overlay_popup, .answer').hide();
+})
+
+$('.questions__link3').click(function() {
+    var popup_id=$('#'+$(this).attr("rel"));
+    $(popup_id).show();
+    $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+    $('.overlay_popup, .answer').hide();
+})
+
+$('.questions__link4').click(function() {
+    var popup_id=$('#'+$(this).attr("rel"));
+    $(popup_id).show();
+    $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+    $('.overlay_popup, .answer').hide();
+})
+
+$('.questions__link5').click(function() {
+    var popup_id=$('#'+$(this).attr("rel"));
+    $(popup_id).show();
+    $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+    $('.overlay_popup, .answer').hide();
+})
+
